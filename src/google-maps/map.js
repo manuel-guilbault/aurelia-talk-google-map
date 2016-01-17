be5c0d94369437e603df6472bde46692aa005460
Manuel Guilbault <manuel.guilbault@gmail.com>
2016-01-17 14:47:08 +0100

    Working map.

--- src/google-maps/map.js
+++ src/google-maps/map.js
@@ -2,19 +2,30 @@ import {
   inject,
   noView
 } from 'aurelia-framework';
-import {Map} from 'google-maps';
+import {Map, LatLng} from 'google-maps';
+
+let nextMapId = 1;
+
+function normalizeElement(element) {
+  if (!element.id) {
+    element.id = "aurelia-google-maps.map-" + nextMapId++;
+  }
+  return element;
+}
 
 @noView
 @inject(Element)
 export class MapCustomElement {
   
   constructor(element) {
-    this.element = element;
+    this.element = normalizeElement(element);
     this.map = this._createMap();
   }
   
   _createMap() {
     return new Map(this.element, {
+      center: new LatLng(0, 0),
+      zoom: 10
     });
   }
 }
